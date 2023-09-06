5acd4f8049c2f9b0a46ffeb3b807baba582d23b9
BookmDan <danoh329@gmail.com>
2023-09-06 09:00:43 -0700

    first try, none of the images are populating

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", function () {
+  
+})
+
+const menu = document.getElementById(ramen-menu);
+fetch('http://localhost:3000/ramens')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((ramen) => {
+      const img = document.createElement('img')
+      img.src = ramens.image
+      // console.log(ramen)
+      // displayData(ramen)
+      menu.appendChild(img)
+    })
+      .catch((err) => {
+        console.error("Error fetching ramen images", err);
+    })
+})
